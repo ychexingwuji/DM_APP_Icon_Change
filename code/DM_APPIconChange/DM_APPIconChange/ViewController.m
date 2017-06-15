0a5cd4ebbced10c9bf7ae928d3add70b6e48774d
wyang <wyang@Ctrip.com>
2017-06-15 20:25:51 +0800

    添加演示工程

---
+++ code/DM_APPIconChange/DM_APPIconChange/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DM_APPIconChange
+//
+//  Created by vyw杨伟(金融) on 2017/6/15.
+//  Copyright © 2017年 brook. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ code/DM_APPIconChange/DM_APPIconChange/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DM_APPIconChange
+//
+//  Created by vyw杨伟(金融) on 2017/6/15.
+//  Copyright © 2017年 brook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
