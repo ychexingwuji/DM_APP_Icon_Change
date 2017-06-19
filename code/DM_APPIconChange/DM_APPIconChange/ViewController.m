93b2da6905c9811b355cf4c30884da524c66ede1
wyang <ychexingwuji@163.com>
2017-06-19 23:47:08 +0800

    更新demo

--- code/DM_APPIconChange/DM_APPIconChange/ViewController.m
+++ code/DM_APPIconChange/DM_APPIconChange/ViewController.m
@@ -10,20 +10,50 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) NSArray<NSString *> *weathers;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.weathers = @[@"fine", @"cloudy", @"sprinkles", @"heavyrain", @"snow", @""];
+    
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setTitle:@"查询今天的天气并更换响应图标" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [button setFrame:CGRectMake(20, 200, self.view.bounds.size.width - 40, 30)];
+    
+    [button addTarget:self action:@selector(btnAction:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:button];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)btnAction:(UIButton *)sender
+{
+    NSString *weather = self.weathers[arc4random() % self.weathers.count];
+    [self setAppIconWithName:weather];
 }
 
+- (void)setAppIconWithName:(NSString *)iconName
+{
+    if (![[UIApplication sharedApplication] supportsAlternateIcons]) {
+        return;
+    }
+    
+    if ([iconName isEqualToString:@""]) {
+        iconName = nil;
+    }
+    
+    [[UIApplication sharedApplication] setAlternateIconName:iconName completionHandler:^(NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"更换app图标发生错误了 ： %@",error);
+        }
+    }];
+}
 
 @end
